033854a7807882aebc7636d876ad2ece5f79dfef
Attogram Project <attogram@users.noreply.github.com>
2017-07-01 04:44:25 +0200

    speed updates, enzo

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 <html><head><title>Testing SQLite ORDER BY RANDOM()</title>
 <meta charset="utf-8" />
 <meta name="viewport" content="initial-scale=1" />
-<?php /*
+<?php /* 
 if( isset($_GET['run']) ) {
-	print '<meta http-equiv="refresh" content="5">';
+   print '<meta http-equiv="refresh" content="1">';
 } */
 ?>
 <style>
-body { 
-	background-color:white; 
-	color:black; 
-	margin:10px 20px 20px 20px; 
-	font-family:sans-serif,helvetica,arial;
+body {
+   background-color:white; 
+   color:black; 
+   margin:10px 20px 20px 20px; 
+   font-family:sans-serif,helvetica,arial;
 }
 h1 { font-size:150%; margin:0px 0px 5px 0px; padding:0px; }
 h2 { font-size:95%; font-weight:normal; margin:0px; padding:0px; }
@@ -21,31 +21,31 @@ a:visited { color:black; background-color:#ffa; }
 a:hover { background-color:yellow; color:black; }
 ul { margin:0px; }
 .results {
-	white-space:pre;
-	font-family:monospace;
-	display:inline-block;
+   white-space:pre;
+   font-family:monospace;
+   display:inline-block;
 }
 .datah {
-	display:inline-block;
-	background-color:lightgrey;
-	color:black;
-	margin:0px;
-	font-weight:bold;
+   display:inline-block;
+   background-color:lightgrey;
+   color:black;
+   margin:0px;
+   font-weight:bold;
 }
 .datab {
-	display:inline-block;
-	background-color:yellow;
-	color:black;
-	padding-left:10px
-	padding-right:10px;
-	margin-bottom:1px;
-	font-weight:bold;
+   display:inline-block;
+   background-color:yellow;
+   color:black;
+   padding-left:10px
+   padding-right:10px;
+   margin-bottom:1px;
+   font-weight:bold;
 }
 .notice {
-	background-color:lightyellow;
-	font-size:90%;
-	border:1px solid black;
-	padding:2px;
+   background-color:lightyellow;
+   font-size:90%;
+   border:1px solid black;
+   padding:2px;
 }
 .pre { white-space:pre; font-family:monospace; font-size:120%; }
 </style>
@@ -56,39 +56,39 @@ require_once( __DIR__.'/random.php' );
 $random = new random();
 
 if( isset($_GET['run']) ) {
-	$nhits = (int)$_GET['run'];
-	if( !$nhits || !is_int($nhits) || $nhits > 1000 ) { $nhits = 1000; }
-	$hit = $random->add_more_random( $nhits );
+   $nhits = (int)$_GET['run'];
+   if( !$nhits || !is_int($nhits) || $nhits > 1000 ) { $nhits = 1000; }
+   $hit = $random->add_more_random( $nhits );
 }
 
 if( isset($_GET['a']) ) {
-	switch( $_GET['a'] ) {
-	
-		case 'erase': // clear all data
-			$random->delete_all_data();
-			break;
-			
-		case 'delete': // DROP TABLE test
-			$random->delete_test_table();
-			break;
-			
-		case 'create': // Create Test Table
-			$size = isset($_GET['size']) ? $_GET['size'] : 10;
-			if( !$size ) { $size = 10; }
-			$random->create_test_table( $size );
-			break;
-	
-		default:
-			print '<p>ERROR: invalid input</p>';
-			break;
-	}	
-}		
+   switch( $_GET['a'] ) {
+   
+      case 'erase': // clear all data
+         $random->delete_all_data();
+         break;
+
+      case 'delete': // DROP TABLE test
+         $random->delete_test_table();
+         break;
+
+      case 'create': // Create Test Table
+         $size = isset($_GET['size']) ? $_GET['size'] : 10;
+         if( !$size ) { $size = 10; }
+         $random->create_test_table( $size );
+         break;
+
+      default:
+         print '<p>ERROR: invalid input</p>';
+         break;
+   }
+}
 ?>
 <div style="float:right;"><a href="./#about">&nbsp;About&nbsp;</a> &nbsp; <a href="./">&nbsp;Refresh&nbsp;</a></div>
 <h1>Testing SQLite ORDER BY RANDOM()</h1>
 <?php /*
 if( isset($_GET['run']) ) {
-	print '<p><a href="./">STOP AUTO RUN</a></p>';
+   print '<p><a href="./">STOP AUTO RUN</a></p>';
  */
 ?>
 <div class="results"><?php 
@@ -108,16 +108,16 @@ print '<br />Table size      : <b>' . number_format(@$info['class_size']) . ' ro
 print '<br /># Data points   : <b>' . number_format(@$info['data_sum']) . '</b>';
 print '<br /># Frequencies   : <b>' . number_format($random->frequencies_count) . '</b>';
 print '<br />Freq Hi/Lo/Range: <b>' 
-	. number_format($random->highest_frequency) 
-	. ' / ' . number_format($random->lowest_frequency) 
-	. ' / ' . number_format( $random->highest_frequency - $random->lowest_frequency )
-	. '</b>';
+   . number_format($random->highest_frequency) 
+   . ' / ' . number_format($random->lowest_frequency) 
+   . ' / ' . number_format( $random->highest_frequency - $random->lowest_frequency )
+   . '</b>';
 print '<br />Rows Hi/Lo/Range: <b>' 
-	. number_format($random->highest_count) 
-	. ' / ' . number_format($random->lowest_count) 
-	. ' / ' . number_format( $random->highest_count - $random->lowest_count )
-	. '</b>';
-	
+   . number_format($random->highest_count) 
+   . ' / ' . number_format($random->lowest_count) 
+   . ' / ' . number_format( $random->highest_count - $random->lowest_count )
+   . '</b>';
+   
 print '<br /><br />' . $distribution_chart;
 
 ?>
@@ -134,12 +134,15 @@ href="?a=create&size=25"> 25</a> <a
 href="?a=create&size=50"> 50</a> <a 
 href="?a=create&size=100"> 100</a> <a 
 href="?a=create&size=250"> 250</a> <a 
-href="?a=create&size=500"> 500</a> rows
+href="?a=create&size=500"> 500</a> <a 
+href="?a=create&size=1000"> 1,000</a> <a 
+href="?a=create&size=10000"> 10,000</a> <a 
+href="?a=create&size=100000"> 100,000</a> rows
 </div>
 
 
-<p><hr /></p>
-
+<br /><br /><br />
+<hr />
 <a name="about"></a>
 
 <h3>About Getting Random with SQLite</h3>
@@ -163,6 +166,10 @@ and the number of rows for each frequency present.
 <p><hr /></p>
 
 </footer>
+
+<p>SQL count: <?php print $random->sql_count; ?></p>
+<p>Fork this: <a href="https://github.com/attogram/random-sqlite-test">https://github.com/attogram/random-sqlite-test</a></p>
+
 </body>
 </html>
 
--- random.php
+++ random.php
@@ -3,262 +3,296 @@
 
 class db {
 
-	var $db;
-	var $database_name;
-	var $sql_count;
-		
-	function init_database() {
-		if( !in_array('sqlite', PDO::getAvailableDrivers() ) ) {
-			return $this->db = FALSE;
-		}
-		try {
-			return $this->db = new PDO('sqlite:'. $this->database_name);
-		} catch(PDOException $e) {
-			return $this->db = FALSE;
-		}
-	}
+    var $db;
+    var $database_name;
+    var $sql_count;
+        
+    function init_database() {
+        if( !in_array('sqlite', PDO::getAvailableDrivers() ) ) {
+            return $this->db = FALSE;
+        }
+        try {
+            return $this->db = new PDO('sqlite:'. $this->database_name);
+        } catch(PDOException $e) {
+            return $this->db = FALSE;
+        }
+    }
 
-	function query_as_array( $sql, $bind=array() ) {
-		if( !$this->db ) { $this->init_database(); }
-		$statement = $this->db->prepare($sql);
+    function query_as_array( $sql, $bind=array() ) {
+        if( !$this->db ) { $this->init_database(); }
+        $statement = $this->db->prepare($sql);
         if( !$statement ) {
             return array();
         }
-		while( $x = each($bind) ) {
-			$statement->bindParam( $x[0], $x[1]);
-		}	
+        while( $x = each($bind) ) {
+            $statement->bindParam( $x[0], $x[1]);
+        }    
         if( !$statement->execute() ) {
             return array();
         }
-		$this->sql_count++;
+        $this->sql_count++;
         $r = $statement->fetchAll(PDO::FETCH_ASSOC);
-		if( !$r && $this->db->errorCode() != '00000') { 
+        if( !$r && $this->db->errorCode() != '00000') {
             $r = array();
-		}
-		return $r;
-	}
+        }
+        return $r;
+    }
 
-	function query_as_bool( $sql, $bind=array() ) {
-		if( !$this->db ) { $this->init_database(); }
-		$statement = $this->db->prepare($sql);
+    function query_as_bool( $sql, $bind=array() ) {
+        if( !$this->db ) { $this->init_database(); }
+        $statement = $this->db->prepare($sql);
         if( !$statement ) {
+            $this->notice('Statement failed: ' . $sql);
             return FALSE;
         }
-		while( $x = each($bind) ) {
-			$statement->bindParam( $x[0], $x[1] );
-		}	
+        while( $x = each($bind) ) {
+            $statement->bindParam( $x[0], $x[1] );
+        }    
         if( !$statement->execute() ) {
+            $this->notice('Execute failed');
             return FALSE;
         }
-		$this->sql_count++;
-		return TRUE;
-	}
-	
-	function vacuum() {
-		return $this->query_as_bool('VACUUM');
-		
-	}
+        $this->sql_count++;
+        return TRUE;
+    }
+    
+    function vacuum() {
+        if( $this->query_as_bool('VACUUM') ) {
+            return TRUE;
+        }
+        $this->notice('ERROR vacumming database');
+        return FALSE;
+    }
 
-	function create_test_table( $size='' ) {
-		if( !$size ) { $size = 10; }
-		if ( !$this->query_as_bool( $this->table[1] ) ) {
-			$this->notice("ERROR creating table 'test'");
-			return FALSE;
-		}
-		for ($i = 1; $i <= $size; $i++) {
-			set_time_limit(120);
-			if( !$this->query_as_bool("INSERT INTO test (id) VALUES ('$i')") ) {
-				$this->notice("ERROR creating rows");
-				return FALSE;
-			}
-		}
-		$this->vacuum();
-		//$this->notice('Created test table with ' . $size . ' rows');
-		return TRUE;
-	}
+    function begin_transaction() {
+        if( $this->query_as_bool('BEGIN TRANSACTION') ) {
+            return TRUE;
+        }
+        $this->notice('ERROR begining transaction');
+        return FALSE;
+    }
 
-	function get_test_table() {
-		if( $this->query_as_array('SELECT * FROM test ORDER BY id') ) {
-			return $table;
-		}
-		return array();
-	}
+    function commit() {
+        if( $this->query_as_bool('COMMIT') ) {
+            return TRUE;
+        }
+        $this->notice('ERROR commiting transaction');
+        return FALSE;
+    }
 
-	function get_test_table_info() {
-		$info = $this->query_as_array('
-			SELECT count(id) AS class_size, sum(frequency) AS data_sum FROM test
-		');
-		if( isset($info[0]) ) {
-			return $info[0];
-		}
-		return array();
-	}
+    function create_test_table( $size='' ) {
+        if( !$size ) { $size = 10; }
+		if( $size > 100000 ) { $size = 100000; }
+        if ( !$this->query_as_bool( $this->table[1] ) ) {
+            $this->notice('ERROR creating table: ' . $this->table[1]);
+            return FALSE;
+        }
+        $this->begin_transaction();
+        for ($i = 1; $i <= $size; $i++) {
 
-	function delete_test_table() {
-		if( $this->query_as_bool('DROP TABLE test;') ) {
-			$this->vacuum();
-			//$this->notice("Dropped table 'test'");
-			return TRUE;
-		}
-		//$this->notice("ERROR dropping table 'test'");
-		return FALSE;
-	}
-	
-	function delete_all_data() {
-		if( $this->query_as_bool('UPDATE test SET frequency = 0;') ) {
-			$this->vacuum();
-			//$this->notice('DELETED all data');
-			return TRUE;
-		}
-		//$this->notice('ERROR deleting all data');
-		return FALSE;
-	}
-	
+            if( !$this->query_as_bool("INSERT INTO test (id) VALUES ('$i')") ) {
+                $this->notice("ERROR creating rows");
+                return FALSE;
+            }
+        }
+        $this->commit();
+        $this->vacuum();
+        //$this->notice('Created test table with ' . $size . ' rows');
+        return TRUE;
+    }
+
+    function get_test_table() {
+        if( $this->query_as_array('SELECT * FROM test ORDER BY id') ) {
+            return $table;
+        }
+        return array();
+    }
+
+    function get_test_table_info() {
+        $info = $this->query_as_array('
+            SELECT count(id) AS class_size, sum(frequency) AS data_sum FROM test
+        ');
+        if( isset($info[0]) ) {
+            return $info[0];
+        }
+        return array();
+    }
+
+    function delete_test_table() {
+        if( $this->query_as_bool('DROP TABLE test;') ) {
+            $this->vacuum();
+            //$this->notice("Dropped table 'test'");
+            return TRUE;
+        }
+        //$this->notice("ERROR dropping table 'test'");
+        return FALSE;
+    }
+
+    function delete_all_data() {
+        if( $this->query_as_bool('UPDATE test SET frequency = 0;') ) {
+            $this->vacuum();
+            //$this->notice('DELETED all data');
+            return TRUE;
+        }
+        //$this->notice('ERROR deleting all data');
+        return FALSE;
+    }
+    
 }
 
 class random extends db {
 
-	var $method;
-	var $table;
-	var $highest_frequency;
-	var $highest_count;
-	var $lowest_frequency;
-	var $lowest_count;
-	var $frequencies_count;
-	
-	function __construct() {
-		$this->database_name = __DIR__ . '/active/test.sqlite';
-		$this->method[1] = 'SELECT id
+    var $method;
+    var $table;
+    var $highest_frequency;
+    var $highest_count;
+    var $lowest_frequency;
+    var $lowest_count;
+    var $frequencies_count;
+
+    function __construct() {
+
+		set_time_limit(30);
+					
+        $this->database_name = __DIR__ . '/active/test.sqlite';
+
+        $this->method[1] = 'SELECT id
 FROM test
 ORDER BY RANDOM()
 LIMIT 1';
-		$this->table[1] = "CREATE TABLE 'test' (
+        $this->table[1] = "CREATE TABLE 'test' (
   'id' INTEGER PRIMARY KEY,
   'frequency' INTEGER DEFAULT '0'
 )";
-	}
 
-	function add_more_random( $size=1 ) {
-		if( !$size || !is_int($size) ) { $size = 1; }
-		if( $size > 1000 ) { $size = 1000; }
-		$hits = array();
-		for ($i = 1; $i <= $size; $i++) {
-			set_time_limit(120);
-			$hit = $this->query_as_array( $this->method[1] );
-			if( !$hit || !isset($hit[0]['id']) ) {
-				$this->notice('ERROR selecting from test table');
-				return FALSE;
-			}
-			$hits[] = $hit[0]['id'];
-		}
-		$freqs = array(); // array of id=>frequency 
-		foreach( $hits as $hit ) {
-			if( isset($freqs[$hit]) ) { 
-				$freqs[$hit] +=1;
-				continue;
-			}
-			$freqs[$hit] = 1;
-		}
-		while( list($id, $frequency) = each($freqs) ) {
-			if( $this->query_as_bool("UPDATE test SET frequency = frequency + $frequency WHERE id = $id") ) {
-				//$this->notice("ADDED random data: id:$id frequency:$frequency");
-				continue;
-			}
-			$this->notice("ERROR adding random data: id:$id frequency:$frequency");
-			return FALSE;
-		}
-		$this->vacuum();
-		return TRUE;
-	}
+		$this->query_as_bool('PRAGMA synchronous=OFF;'); // do not wait for disk writes
 
-	function display_results() {
-		$count = 0;
-		$display = '<div class="results">';
-		foreach( $this->get_test_table() as $data ) {
-			$count++;
-			$width = $data['frequency'];
-			$display .= ''
-				. '<div class="datah">' 
-				. $data['id']
-				. '</div><div class="datab" style="width:' . $width . 'px;">' 
-				. $data['frequency'] . '</div>'
-				. "\n"
-				;
-			if( $count % 35	== 0 ) {
-				$display .= '</div><div class="results">';
-			}
-		}
-		$display .= '</div>';
-		$display = preg_replace('#<div class="results"></div>$#','',$display);
-		return $display;
-		//return print_r($this->get_test_table(),1);
-	}
+		$this->query_as_bool('PRAGMA count_changes=OFF;'); // do not do callback to count changes per query
 
-	function get_total_data_points() {
-		$data = $this->query_as_array('
-			SELECT SUM(frequency) AS frequency FROM test
-		');
-		if( !$data|| !isset($data[0]['frequency']) ) {
-			return 'NULL';
-		}
-		return $data[0]['frequency'];
-	}
-	
-	function display_distribution() {
-		$display = ''
-			. '<div class="datah" style="background-color:darkgrey; color:black;"> Frequency </div>'
-			. '<div class="datab" style="background-color:lightgrey;color:black;"> Rows      </div>'
-			. '<br />';
-		foreach( $this->get_distribution() as $dist ) {
-			$cwidth = 1 * $dist['count'];
-			if( $cwidth < 1 ) { $cwidth = 1; }
-			//if( $cwidth > 500 ) { $cwidth = 500; }
+	} // end __construct()
 
-			$display .= ''
-				. '<div class="datah">' 
-					. str_pad(number_format($dist['frequency']), 10, ' ', STR_PAD_BOTH)
-				. ' </div>'
-				. '<div class="datab" style="width:' 
-					. $cwidth . 'px;"> ' 
-					. number_format($dist['count']) . '</div>'
-				. '<br />';
-				;
-		}			
-		//$display .= '</div>';
-		return $display;
-	}
+    function add_more_random( $size=1 ) {
+        if( !$size || !is_int($size) ) { $size = 1; }
+        if( $size > 1000 ) { $size = 1000; }
+        $hits = array();
+        for ($i = 1; $i <= $size; $i++) {
+            $hit = $this->query_as_array( $this->method[1] );
+            if( !$hit || !isset($hit[0]['id']) ) {
+                $this->notice('ERROR selecting from test table');
+                return FALSE;
+            }
+            $hits[] = $hit[0]['id'];
+        }
+        $freqs = array(); // array of id=>frequency 
+        foreach( $hits as $hit ) {
+            if( isset($freqs[$hit]) ) { 
+                $freqs[$hit] +=1;
+                continue;
+            }
+            $freqs[$hit] = 1;
+        }
+        $this->begin_transaction();
+        while( list($id, $frequency) = each($freqs) ) {
+            if( $this->query_as_bool("UPDATE test SET frequency = frequency + $frequency WHERE id = $id") ) {
+                //$this->notice("ADDED random data: id:$id frequency:$frequency");
+                continue;
+            }
+            $this->notice("ERROR adding random data: id:$id frequency:$frequency");
+            return FALSE;
+        }
+        $this->commit();
+        $this->vacuum();
+        return TRUE;
+    }
 
-	function get_distribution() {
-		$dist = $this->query_as_array('
-			SELECT frequency, count(id) AS count
-			FROM test
-			GROUP BY frequency
-			ORDER BY frequency DESC
-		');
-		if( !$dist ) { return array(); }
-		
-		$flow = $fhigh = NULL;
-		$clow = $chigh = NULL;
-		foreach( $dist as $d ) {
-			if( !$flow ) { $flow = $d['frequency']; }
-			if( !$clow ) { $clow = $d['count']; }
-			if( !$fhigh ) { $fhigh = $d['frequency']; }
-			if( !$chigh ) { $chigh = $d['count']; }
-			if( $d['frequency'] > $fhigh ) { $fhigh = $d['frequency']; }
-			if( $d['count'] > $chigh ) { $chigh = $d['count']; }
-			if( $d['frequency'] < $flow ) { $flow = $d['frequency']; }
-			if( $d['count'] < $clow ) { $clow = $d['count']; }
-		}
-		$this->highest_frequency = $fhigh;
-		$this->highest_count = $chigh;
-		$this->lowest_frequency = $flow;
-		$this->lowest_count = $clow;
-		$this->frequencies_count = sizeof($dist);
-		return $dist;
-	}
-	
+/*    function display_results() {
+        $count = 0;
+        $display = '<div class="results">';
+        foreach( $this->get_test_table() as $data ) {
+            $count++;
+            $width = $data['frequency'];
+            $display .= ''
+                . '<div class="datah">' 
+                . $data['id']
+                . '</div><div class="datab" style="width:' . $width . 'px;">' 
+                . $data['frequency'] . '</div>'
+                . "\n"
+                ;
+            if( $count % 35    == 0 ) {
+                $display .= '</div><div class="results">';
+            }
+        }
+        $display .= '</div>';
+        $display = preg_replace('#<div class="results"></div>$#','',$display);
+        return $display;
+        //return print_r($this->get_test_table(),1);
+    } */
 
-	function notice( $msg ) {
-		print '<div class="notice pre">' . print_r($msg,1) . '</div>';
-	}
+    function get_total_data_points() {
+        $data = $this->query_as_array('
+            SELECT SUM(frequency) AS frequency FROM test
+        ');
+        if( !$data|| !isset($data[0]['frequency']) ) {
+            return 'NULL';
+        }
+        return $data[0]['frequency'];
+    }
+
+    function display_distribution() {
+        $display = ''
+            . '<div class="datah" style="background-color:darkgrey; color:black;"> Frequency </div>'
+            . '<div class="datab" style="background-color:lightgrey;color:black;"> Rows      </div>'
+            . '<br />';
+        foreach( $this->get_distribution() as $dist ) {
+            $cwidth = 1 * $dist['count'];
+            if( $cwidth < 1 ) { $cwidth = 1; }
+            //if( $cwidth > 500 ) { $cwidth = 500; }
+
+            $display .= ''
+                . '<div class="datah">' 
+                    . str_pad(number_format($dist['frequency']), 10, ' ', STR_PAD_BOTH)
+                . ' </div>'
+                . '<div class="datab" style="width:' 
+                    . $cwidth . 'px;"> ' 
+                    . number_format($dist['count']) . '</div>'
+                . '<br />';
+                ;
+        }            
+        //$display .= '</div>';
+        return $display;
+    }
+
+    function get_distribution() {
+        $dist = $this->query_as_array('
+            SELECT frequency, count(id) AS count
+            FROM test
+            GROUP BY frequency
+            ORDER BY frequency DESC
+        ');
+        if( !$dist ) { return array(); }
+        
+        $flow = $fhigh = NULL;
+        $clow = $chigh = NULL;
+        foreach( $dist as $d ) {
+            if( !$flow ) { $flow = $d['frequency']; }
+            if( !$clow ) { $clow = $d['count']; }
+            if( !$fhigh ) { $fhigh = $d['frequency']; }
+            if( !$chigh ) { $chigh = $d['count']; }
+            if( $d['frequency'] > $fhigh ) { $fhigh = $d['frequency']; }
+            if( $d['count'] > $chigh ) { $chigh = $d['count']; }
+            if( $d['frequency'] < $flow ) { $flow = $d['frequency']; }
+            if( $d['count'] < $clow ) { $clow = $d['count']; }
+        }
+        $this->highest_frequency = $fhigh;
+        $this->highest_count = $chigh;
+        $this->lowest_frequency = $flow;
+        $this->lowest_count = $clow;
+        $this->frequencies_count = sizeof($dist);
+        return $dist;
+    }
+
+    function notice( $msg ) {
+        print '<div class="notice pre">' . print_r($msg,1) . '</div>';
+    }
+
 }
